befada662f85f1e1fef63492fbd4ddf402e2e788
robeusanio11 <robeusanio11@gmail.com>
2023-10-01 01:45:02 -0700

    database WIP/dynamic rendering

--- nbatest/models.py
+++ nbatest/models.py
@@ -2,3 +2,11 @@ from django.db import models
 
 # Create your models here.
 
+class Teams(models.Model):
+    id = models.IntegerField(primary_key="true")
+    abbr = models.CharField(max_length=3, null="true")
+
+class Roster(models.Model):
+    id = models.IntegerField(primary_key="true");
+    teamID = models.ForeignKey(Teams, on_delete=models.CASCADE)
+    playerName = models.CharField(max_length=50)
--- nbatest/views.py
+++ nbatest/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render, HttpResponse
+from nbatest.models import Teams, Roster
+from bs4 import BeautifulSoup
+import requests
 
 # Create your views here.
 def home(req):
-    return render(req, "teams.html")
+    # teams = Teams.objects.all()
+    # idCount = 0
+    teams = ["ATL", "BOS", "BRK", "CHO", "CHI", "CLE", "DAL", "DEN", "DET", "GSW", "HOU", "IND", "LAC", "LAL", "MEM", "MIA", "MIL", "MIN", "NOH", "NYK", "OKC", "ORL", "PHI", "PHO", "POR", "SAC", "SAS", "TOR", "UTA", "WAS"]
+    # for team in teams:
+        # teamObject = Teams(id=idCount, abbr=team)
+        # teamObject.save()
+        # html_text = requests.get(f"https://www.basketball-reference.com/teams/{team}/2024.html").text;
+        # soup = BeautifulSoup(html_text, 'lxml');
+        # player_cards = soup.find_all('td', attrs={"data-stat": "player"});
+        # html_text = requests.get(f"https://www.basketball-reference.com/teams/{team}/2024.html").text
+    return render(req, "teams.html", {"teams": teams})
