7ae42d519e8a61e354da0f9492949728d1656e74
robeusanio11 <robeusanio11@gmail.com>
2023-10-01 02:50:33 -0700

    display scraping all teams WIP

--- nbatest/views.py
+++ nbatest/views.py
@@ -7,12 +7,25 @@ import requests
 def home(req):
     # teams = Teams.objects.all()
     # idCount = 0
-    teams = ["ATL", "BOS", "BRK", "CHO", "CHI", "CLE", "DAL", "DEN", "DET", "GSW", "HOU", "IND", "LAC", "LAL", "MEM", "MIA", "MIL", "MIN", "NOH", "NYK", "OKC", "ORL", "PHI", "PHO", "POR", "SAC", "SAS", "TOR", "UTA", "WAS"]
+    teamAbbrs = ["ATL", "BOS", "BRK", "CHO", "CHI", "CLE", "DAL", "DEN", "DET", "GSW", "HOU", "IND", "LAC", "LAL", "MEM", "MIA", "MIL", "MIN", "NOH", "NYK", "OKC", "ORL", "PHI", "PHO", "POR", "SAC", "SAS", "TOR", "UTA", "WAS"]
     # for team in teams:
         # teamObject = Teams(id=idCount, abbr=team)
         # teamObject.save()
-        # html_text = requests.get(f"https://www.basketball-reference.com/teams/{team}/2024.html").text;
-        # soup = BeautifulSoup(html_text, 'lxml');
-        # player_cards = soup.find_all('td', attrs={"data-stat": "player"});
-        # html_text = requests.get(f"https://www.basketball-reference.com/teams/{team}/2024.html").text
-    return render(req, "teams.html", {"teams": teams})
+    teams = []
+    for team in teamAbbrs:
+        teamObj = {
+            "name": team,
+            "players": [],
+        }
+        html_text = requests.get(f"https://www.basketball-reference.com/teams/{team}/2024.html").text
+        soup = BeautifulSoup(html_text, 'lxml')
+        player_cards = soup.find_all('td', attrs={"data-stat": "player"})
+        playerList = []
+        for player in player_cards:
+            print(player)
+            playerList.append(player.find('a').text)
+        # print(playerList)
+        teamObj["players"] = playerList
+        # print(teamObj)
+        teams.append(teamObj)
+    return render(req, "teams.html", {"teams":teams})
